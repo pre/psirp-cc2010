84ab3e05c1b3b50c5595e9a9f98922f66b0599c1
Petrus Repo <petrus.repo@iki.fi>
2010-05-14 10:21:26 +0300

    ready, report still to be finished

--- misc/yield2.py
+++ misc/yield2.py
@@ -1,11 +1,11 @@
 
-def summaa(alkio):
+def summaa(alkio, testi):
   for toisto in range(1, 3):
     yield(str(toisto) + "alkio on " + str(alkio))
   
 def iteraattori():
   for alkio in range(1, 5):
-    yield(summaa(alkio))
+    yield(summaa(alkio), "testi")
 #    yield(alkio)
     
 for i in iteraattori():
