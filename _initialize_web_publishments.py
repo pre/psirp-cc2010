60193a1810c01c329f980bf339ab0a36b7195efa
Petrus Repo <petrus.repo@iki.fi>
2010-06-08 12:15:18 +0300

    init web publishments

---
+++ _initialize_web_publishments.py
@@ -0,0 +1,9 @@
+from publishment import *
+
+Publishment('{"status": "SEAT_AVAILABLE"}', "::aa","::a1")
+Publishment('{"status": "SEAT_AVAILABLE"}', "::aa","::a2")
+Publishment('{"status": "SEAT_AVAILABLE"}', "::aa","::a3")
+Publishment('{"status": "SEAT_AVAILABLE"}', "::aa","::a4")
+Publishment('{"status": "SEAT_AVAILABLE"}', "::aa","::a5")
+Publishment('{"status": "SEAT_AVAILABLE"}', "::aa","::a6")
+Publishment('{"message": ""}', "::aa","::bb")
--- start_server.sh
+++ start_server.sh
@@ -6,6 +6,9 @@ export PYTHONPATH=/home/psirp/psirp
 echo "Removing OS X crappy files"
 rm -vf public/websocket/._*
 
+echo "Initializing seats (fixme: this should not be needed, read Subscriber#listen())"
+python2.6 _initialize_web_publishments.py
+
 echo "Starting server"
 cd mod_pywebsocket && \
 python2.6 standalone.py -p 80 -d ../public --log-level=debug -m websocket/handler_map.txt -w websocket --allow-draft75 
